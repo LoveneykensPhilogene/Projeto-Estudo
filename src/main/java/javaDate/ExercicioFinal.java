531913b6c8209b043331d9338540ee0ed8439707
Loveneykens Philogene <loveneykens@gmail.com>
2021-03-07 14:56:09 -0300

    "Reajusta no arquivo ExerciçoFinal"

--- src/main/java/javaDate/ExercicioFinal.java
+++ src/main/java/javaDate/ExercicioFinal.java
@@ -1,5 +1,8 @@
 package javaDate;
 
+import javax.swing.text.DateFormatter;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.function.LongBinaryOperator;
@@ -7,19 +10,26 @@ import java.util.function.LongBinaryOperator;
 public class ExercicioFinal {
     public static void main(String[]args){
         //instancia
-       // Calendar calendar=new Calendar(2010,HAITI);
+        @Deprecated
+        Date meudataNascimento=new Date(94,02,10);
+        //retorna timeMillis do tipo long
+        Long timeMillisNascimento= meudataNascimento.getTime();
+        //passa valor do tipo long como parametro
+        Date novoNascimento=new Date(timeMillisNascimento);
+         //cria um novo objeto
+        @Deprecated
+        Date novaData=new Date(110,04,15);
+        //verifica se a data é anterior
+        boolean dataAnterior=novaData.after(novoNascimento);
+        //verifica se a data é posterior
+        boolean dataPosterior=novaData.before(novoNascimento);
+        //imprime ou mostra
+        System.out.println("timeInMillis : "+timeMillisNascimento);
+        System.out.println("O objeto Date : "+novoNascimento);
+        System.out.println("Minha data de nascimento é Anterior : "+dataAnterior);
+        System.out.println("Minha data de nascimento é posterior : "+dataPosterior);
 
-        @Deprecated
-        Date meudataNascimento=new Date(94,03,10);
-        Long currentgetTime= meudataNascimento.getTime();
-        Date dateNovo=new Date(currentgetTime);
-        @Deprecated
-        Date novoDate=new Date(+2010,05,15);
-        Long currentNovoDate= novoDate.getTime();
-        Date novaDateAtual=new Date(currentNovoDate);
-        System.out.println(dateNovo);
-        System.out.println(currentgetTime);
-        System.out.println(novaDateAtual);
+
 
 
 
